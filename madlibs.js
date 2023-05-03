b7c45544bff09ef0909cf8642cf31fa3c514cf6c
Medetcan Diler <medetdiler1996@gmail.com>
2023-05-03 21:23:49 +0300

    Saving the process

--- madlibs.js
+++ madlibs.js
@@ -26,10 +26,91 @@
  * There are multiple ways to do this, but you may want to use regular expressions.
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
+
+const edit = document.querySelector('.madLibsEdit');
+const previer = document.querySelector('.madLibsPreview');
+
+// create input 
+function createInput( type, index ){
+  const input = document.createElement('input');
+  input.setAttribute('placeholder', type);
+  input.setAttribute('class', 'input')
+
+  edit.appendChild(input)
+  //your code here  
+  const inputs = document.getElementsByClassName('input');
+  const inputsArr = Array.from(inputs)
+  inputsArr.map( input => {
+    input.addEventListener('change', e => {
+      //codes
+      console.log(createWordObject( arr ))
+      console.log(e.target.value)
+    })
+  })
+}
+
+
+
+function createText(word){
+  edit.innerHTML += ` ${word} `
+}
+
+function createPreviewText(word){
+
+  previer.innerHTML += ` ${word} `
+}
+
 function parseStory(rawStory) {
   // Your code here.
-  return {}; // This line is currently wrong :)
+  const newArr = rawStory.split(' ');
+  const words = createWordObject(newArr);
+  console.log(newArr)
+  return words; 
 }
+
+const story = [];
+
+function createWordObject( arr ){
+
+  arr.forEach(item => story.push(item) )
+  console.log(story, 'asdfasdfasdfasdf')
+  ourStory = arr.map(item => {
+    const myObject = {};
+    if( item.includes('[n]')){
+      myObject.pos = 'noun';
+      let restoredItem = item.replace('[n]','')
+      myObject.word = restoredItem;
+      //
+      createInput('noun'); 
+      createPreviewText(myObject.word)
+    } else if (item.includes('[v]')){
+      myObject.pos = 'verb';
+      let restoredItem = item.replace('[v]','')
+      myObject.word = restoredItem;
+      //
+      createInput('verb') 
+      createPreviewText(myObject.word)
+    } else if ( item.includes('[a]')){
+      myObject.pos = 'adjactive'
+      let restoredItem = item.replace('[a]','')
+      myObject.word = restoredItem;
+      //
+      createInput('adj') 
+      createPreviewText(myObject.word)
+    } else {
+      myObject.word = item;
+      createText(myObject.word);
+      createPreviewText(myObject.word)
+    }
+    return myObject;
+  });
+
+  return ourStory
+}
+
+
+
+
 
 /**
  * All your other JavaScript code goes here, inside the function. Don't worry about
@@ -42,8 +123,12 @@ function parseStory(rawStory) {
  *
  * You'll want to use the results of parseStory() to display the story on the page.
  */
+
+
+
 getRawStory()
   .then(parseStory)
   .then((processedStory) => {
     console.log(processedStory);
   });
+
